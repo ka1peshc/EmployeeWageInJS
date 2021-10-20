bfd258893902bbd4c8ea43fa4511b12abdce72bc
ka1peshc <kalpesh111997@gmail.com>
2021-10-20 17:15:10 +0530

    Add: while loop to check maximum day or total working hours satisfy condition

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -16,6 +16,8 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NO_OF_WORKING_DAYS = 20;
+const MAX_WORKING_HR = 160;
 function getWorkingHour(empCheck){
     switch(empCheck){
         case IS_PART_TIME:
@@ -27,7 +29,13 @@ function getWorkingHour(empCheck){
     }
 }
 
-let empCheck = Math.floor(Math.random() * 10) % 3;
-let empHrs = getWorkingHour(empCheck);
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Hours worked "+empHrs+" Emp wage: "+empWage);
+
+let totalHrs = 0;
+let totalWorkingdays=0;
+while( totalHrs <= MAX_WORKING_HR && totalWorkingdays < NO_OF_WORKING_DAYS){
+    totalWorkingdays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalHrs += getWorkingHour(empCheck);
+}
+let empWage = totalHrs * WAGE_PER_HOUR;
+console.log(" Total days "+totalWorkingdays+" Hours worked "+totalHrs+" Emp wage: "+empWage);

cbef7328562a2de4b67556c1c0ba3a517747f472
ka1peshc <kalpesh111997@gmail.com>
2021-10-20 16:27:58 +0530

    Add: Calculating pay according to work hour

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,7 +1,35 @@
-const IsABSENT = 0;
-let empCheck = Math.floor(Math.random() * 10)%2;
-if(empCheck == IsABSENT){
-    console.log("ABSENT");
-}else{
-    console.log("PRESENT");
+//UC1- IF ELSE CONDITION 
+{
+    const IsABSENT = 0;
+    let empCheck = Math.floor(Math.random() * 10)%2;
+    if(empCheck == IsABSENT){
+        console.log("Employee is ABSENT. Exiting program");
+        
+    }else{
+        console.log("PRESENT");
+    }
+}
+
+//UC2 SWITCH STATEMENT
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+{
+    let empHrs = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    switch(empCheck){
+        case IS_PART_TIME:
+            empHrs=PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            empHrs=FULL_TIME_HOURS;
+            break;
+        default:
+            empHrs=0;
+    }
+
+    let empWage = empHrs * WAGE_PER_HOUR;
+    console.log("UC2- Emp wage: "+empWage);
 }

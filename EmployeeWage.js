0daece6fe64d4007f143dfb66d628a31f392df44
ka1peshc <kalpesh111997@gmail.com>
2021-10-22 17:28:21 +0530

    Add: gender and starte date property and used toLocaleDateString

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -175,12 +175,16 @@ class EmployeePayrollData {
     //property
     id;
     salary;
+    gender;
+    startDate;
 
     //constructor
-    constructor(id, name, salary){
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    constructor(...params){
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
     //getter and setter
@@ -189,7 +193,9 @@ class EmployeePayrollData {
 
     //method
     toString(){
-        return "id ="+this.id+", name = "+this.name+", salary = "+this.salary;
+        const option = { year: 'numeric', month : 'long', day : 'numeric'};
+        const empDate = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-US",option);
+        return "id ="+this.id+", name = "+this.name+", salary = "+this.salary+", gender = "+this.gender+", startDate "+empDate;
     }
 }
 
@@ -197,3 +203,5 @@ let employeePayrollData = new EmployeePayrollData(1,"Kalpesh",50000);
 console.log(employeePayrollData.toString());
 employeePayrollData.name = "John";
 console.log(employeePayrollData.toString());
+let newemployeePayrollData = new EmployeePayrollData(2,"Terissa",70000,"Female",new Date());
+console.log(newemployeePayrollData.toString());

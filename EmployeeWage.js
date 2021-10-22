1749c64102308f6d3bee9fd3e99c837e7a6b251e
ka1peshc <kalpesh111997@gmail.com>
2021-10-22 17:15:48 +0530

    Add: Employee payroll class

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -170,4 +170,30 @@ let nonworkingDayStrArr = empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyH
 .forEach(dailyHrsAndWage => dailyHrsAndWage.toString());
 console.log("\nUC 11D No working days "+nonworkingDayStrArr);
 
+//UC11 Employee payroll class
+class EmployeePayrollData {
+    //property
+    id;
+    salary;
 
+    //constructor
+    constructor(id, name, salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    //getter and setter
+    get name() { return this._name;}
+    set name(name) {this._name = name;}
+
+    //method
+    toString(){
+        return "id ="+this.id+", name = "+this.name+", salary = "+this.salary;
+    }
+}
+
+let employeePayrollData = new EmployeePayrollData(1,"Kalpesh",50000);
+console.log(employeePayrollData.toString());
+employeePayrollData.name = "John";
+console.log(employeePayrollData.toString());
